61c508f05abccc4a0986504eecb5d192fee03a70
mastercooder <tejesh.patel256@gmail.com>
2024-09-10 14:28:33 +0530

    Update day5.py

--- 2nd Year/3rd Semester/Programming in Python/Code/day5.py
+++ 2nd Year/3rd Semester/Programming in Python/Code/day5.py
@@ -1,5 +1,27 @@
-user = int(input("Enter how much lines you want to print: "))
-for i in range(1, user):
-    for j in range(i):
-        print("*", end="")
+while(True):
+    print('''
+    1,  1
+        2, 4
+        3, 6, 9
+        4, 8, 12, 16
+    
+    2. 1, 2, 3, 4, 5
+    
+          ''')
+    print("-----------------------------------------------")
     print()
+
+    query = int(input("Enter the problem Number: "))
+    
+    if(query==1):
+        for i in range(1, 6):
+            for j in range(i):
+                print(i, end="")
+            print()
+    elif(query==2):
+        a = 0
+        for i in range(1, 6):
+            for j in range(i):
+                a += 1
+                print(a, end="")
+            print()
